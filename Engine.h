e7ea309260b73c67e3c0696dc77265ade8f7e854
Mihir Sharma <mihirsharmaofficial@gmail.com>
2020-04-26 14:56:20 +0530

    Single include file for all functions and classes
    remove source.cpp and compile to .lib or .dll with Engine.h to use in seperate projects

---
+++ Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include<iostream>
+#include "VectorOps.h"
+#include "GameObject.h"
+#include<chrono>
+#include "Physics.h"
+#include<conio.h>
+#include<typeinfo>
+#define clrscr std::cout << "\033[2J\033[1;1H"
